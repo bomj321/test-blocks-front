d2f3b150f10ac593993428fadd328039c39c7551
bomj321 <jmobb612@gmail.com>
2021-10-04 07:33:23 -0400

    [DELETE] Trash code

--- src/hooks/decodeToken.js
+++
@@ -1,7 +0,0 @@
-export default function decodeToken() {
-  const tokenString = localStorage.getItem("token");
-
-  return tokenString
-    ? JSON.parse(atob(tokenString.split(".")[1]))
-    : { role: "NOT_PERMITED" };
-}
